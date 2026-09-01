273c755077f48c51daa74ceb5aa5417c24dd20cb
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2026-09-01 06:50:35 +0530

    update

--- stdex.cs
+++ stdex.cs
@@ -27,9 +27,12 @@
  * SOFTWARE.
  * */
 
+#nullable enable
+
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace stdex;
 
@@ -39,8 +42,22 @@ namespace stdex;
 /// already present elements, resulting in an always ordered collection.
 /// </summary>
 class OrderedList<T> where T: IComparable<T> {
+
 	public OrderedList() {}
+
 	private readonly List<T> _list = [];
+	public int Count { get { return _list.Count; }}
+	public T this[int i] {
+		get {
+			return _list[i];
+		}
+		set {
+			_list[i] = value;
+		}
+	}
+
+	public bool Contains(T a) => _list.Contains(a);
+
 	public void Add(T item) {
 		if(Count > 0) {
 			for(int i = 0; i < Count; i++) {
@@ -64,33 +81,38 @@ class OrderedList<T> where T: IComparable<T> {
 		}
 		else _list.Add(item);
 	}
-	public T this[int i] {
-		get {
-			return _list[i];
+
+	public override bool Equals(object? a) {
+		if(a is null) return this is null;
+		if(a is OrderedList<T> _a) {
+			if(base.Equals(_a)) return true;
+			if(_a.Count != Count) return false;
+			for(int i = 0; i < Count; i++) {
+				if(_a[i].CompareTo(this[i]) != 0)
+					return false;
+			}
+			return true;
 		}
-		set {
-			_list[i] = value;
-		}
+		return false;
 	}
 
-	public bool Contains(T a) => _list.Contains(a);
+    public override int GetHashCode()
+    {
+		unchecked {
+			int hash = 17;
+			hash = hash * 23 + _list.GetHashCode();
+			hash = hash * 23 + Count.GetHashCode();
 
-	public static bool operator ==(OrderedList<T> a, OrderedList<T> b) {
-		if(a.Count != b.Count)
-			return false;
-		for(int i = 0; i < a.Count; i++) {
-			if(a[i].CompareTo(b[i]) != 0)
-				return false;
+			return hash;
 		}
-		return true;
-	}
-	public static bool operator !=(OrderedList<T> a, OrderedList<T> b) {
-		return !(a == b);
-	}
-	public int Count { get { return _list.Count; }}
+    }
+
+	public static bool operator ==(OrderedList<T> a, OrderedList<T> b) => a.Equals(b);
+	
+	public static bool operator !=(OrderedList<T> a, OrderedList<T> b) => !a.Equals(b);
 
 	public override string ToString() {
-		string text = $"OrderedList<{typeof(T).Name}>(count: {Count})[";
+		string text = $"OrderedList<{typeof(T).Name}>({Count})[";
 		for(int i = 0; i < Count; i++) {
 			text += $"{_list[i]}";
 			if(i != Count - 1) text += ", ";
@@ -100,10 +122,39 @@ class OrderedList<T> where T: IComparable<T> {
 	}
 }
 
+/// <summary>
+/// Extensions on built-in types go here
+/// </summary>
 static class Extensions {
 	extension(string) {
 		public static string operator *(string a, int n) {
 			return string.Concat(Enumerable.Repeat(a, n));
 		}
 	}
+}
+
+/// <summary>
+/// Hardware clocks
+/// </summary>
+class Clock {
+	public class Now {
+		private static long freq = 0;
+		public static double Time() {
+			if (freq == 0)
+				Kernel32.QueryPerformanceFrequency(out freq);
+			Kernel32.QueryPerformanceCounter(out long timeStamp);
+			return (double)timeStamp / freq;
+		}
+		public static long Seconds() => (long)Time();
+		public static long Milli() => (long)(Time()*1000);
+		public static long Micro() => (long)(Time()*1000000);
+	}
+}
+
+class Kernel32 {
+	[DllImport("kernel32.dll")]
+    public static extern int QueryPerformanceFrequency(out long freq);
+
+    [DllImport("kernel32.dll")]
+    public static extern int QueryPerformanceCounter(out long timeStamp);
 }
